38fe0829ee602be5fc384e721398fcdab1d81b8c
MohammedAsaadAsaad <m.a.asaad@hotmail.com>
2022-04-04 20:32:10 +0300

    add isMapOf

--- lib/validation/lists.dart
+++ lib/validation/lists.dart
@@ -5,6 +5,11 @@ extension ListsValidationExtention on ApiValidator {
   IsListOfType<T> isListOfType<T>() {
     return IsListOfType<T>()..parent = this;
   }
+
+  /// Add [IsMapOf] validation rule.
+  IsMapOf<K, V> isMapOf<K, V>() {
+    return IsMapOf<K, V>()..parent = this;
+  }
 }
 
 class IsListOfType<T> extends ApiValidator {
@@ -14,6 +19,22 @@ class IsListOfType<T> extends ApiValidator {
     if (value is! List) return '[$fieldName] must be a list';
     for (var v in value) {
       if (v is! T) return '[$fieldName] has invalid values';
+    }
+    return null;
+  }
+}
+
+class IsMapOf<K, V> extends ApiValidator {
+  @override
+  String? validateValue(String fieldName, value) {
+    if (value == null) return null;
+    if (value is! Map) {
+      return '[$fieldName] must be a Map of ${K.toString()},${V.toString()}';
+    }
+    for (var v in value.entries) {
+      if (v.key is! K || v.value is! V) {
+        return '[$fieldName] has invalid entries';
+      }
     }
     return null;
   }
